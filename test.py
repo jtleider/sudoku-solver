61caca1f5d205b74567b3292b48b46c4066cc424
jtleider <jtleider@gmail.com>
2014-08-21 12:08:07 -0500

    Added Grid class getCell method tests.

--- test.py
+++ test.py
@@ -114,6 +114,19 @@ class GridTest(unittest.TestCase):
 	grid.setCell(0, 0, 9)
 	self.assertTrue(grid.isSolved())
 
+    def test_getCell(self):
+	grid = Grid([[0, 6, 3, 1, 7, 4, 2, 5, 8]]+
+			[[1, 7, 8, 3, 2, 5, 6, 4, 9]] +
+			[[2, 5, 4, 6, 8, 9, 7, 3, 1]] +
+			[[8, 2, 1, 4, 3, 7, 5, 9, 6]] +
+			[[4, 9, 6, 8, 5, 2, 3, 1, 7]] +
+			[[7, 3, 5, 9, 6, 1, 8, 2, 4]] +
+			[[5, 8, 9, 7, 1, 3, 4, 6, 2]] +
+			[[3, 1, 7, 2, 4, 6, 9, 8, 5]] +
+			[[6, 4, 2, 5, 9, 8, 1, 7, 3]])
+	self.assertEqual(grid.getCell(8, 2), 2)
+	self.assertEqual(grid.getCell(0, 0), 0)
+	
     def test_findEmpty(self):
 	grid = Grid([[0, 6, 3, 1, 7, 4, 2, 5, 8]]+
 			[[1, 7, 8, 3, 2, 5, 6, 4, 9]] +
@@ -156,7 +169,6 @@ class GridTest(unittest.TestCase):
 			[[6, 4, 2, 5, 9, 8, 1, 7, 3]])
 	self.assertEqual(grid.findEmpty(), None)
 
-	
 	
 if __name__ == '__main__':
     unittest.main()
